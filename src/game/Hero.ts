b866ac480fc1322eb6cb9dea3a3c922cb1ef9e02
Lukas Ruebbelke <lukas@presolved.io>
2023-02-02 18:16:32 -0700

    update: animations

--- src/game/Hero.ts
+++ src/game/Hero.ts
@@ -2,9 +2,11 @@ import { Play } from './Play';
 
 export class Hero extends Phaser.Physics.Arcade.Sprite {
   keys!: Phaser.Types.Input.Keyboard.CursorKeys;
+  animations!: any;
   
   constructor(scene: Play, x, y) {
     super(scene, x, y, 'hero');
+    this.animations = scene.getAnimations('hero');
     this.setOrigin(0.5, 0.5);
     this.initKeys(scene);
   }
@@ -19,6 +21,11 @@ export class Hero extends Phaser.Physics.Arcade.Sprite {
     } else {
       this.halt();
     }
+
+    let animationName = this.getAnimationName();
+    if (this.anims.getName() !== animationName) {
+      this.anims.play(animationName);
+    }
   }
 
   jump() {
@@ -28,16 +35,32 @@ export class Hero extends Phaser.Physics.Arcade.Sprite {
 
   runRight() {
     this.setVelocityX(160);
+    this.flipX = false;
   }
 
   runLeft() {
     this.setVelocityX(-160);
+    this.flipX = true;
   }
 
   halt() {
     this.setVelocityX(0);
   }
 
+  getAnimationName() {
+    let name = this.animations.stop; // default animation
+
+    if (this.body.velocity.y > 0) {
+      name = this.animations.jump;
+    } else if (this.body.velocity.y <= 0 && !this.body.touching.down) {
+      name = this.animations.fall;
+    } else if (this.body.velocity.x !== 0 && this.body.touching.down) {
+      name = this.animations.run;
+    }
+
+    return name;
+  }
+  
   private initKeys(scene) {
     this.keys = scene.input.keyboard.createCursorKeys();
   }
